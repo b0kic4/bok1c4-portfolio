e898460db1574cb907d91258dafe16ef74705cfd
Boris Nikolic <borisnikolic2302@gmail.com>
2024-02-26 04:17:20 +0100

    simple portfolio website

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,35 +1,36 @@
+import Link from "next/link";
 import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
 export default function Sidebar() {
   return (
     <div className="fixed top-0 left-0 h-full bg-transparent p-4 flex flex-col items-center justify-center">
-      <a
-        href="https://github.com/your-github-profile"
+      <Link
+        href="https://github.com/b0kic4"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white mb-4"
       >
         <FaGithub size={24} />
-      </a>
-      <a
-        href="https://linkedin.com/in/your-linkedin-profile"
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/boris-nikolic-a44a2126a/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white mb-4"
       >
         <FaLinkedin size={24} />
-      </a>
-      <a
-        href="https://twitter.com/your-twitter-handle"
+      </Link>
+      <Link
+        href="https://twitter.com/bok1c4"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white mb-4"
       >
         <FaTwitter size={24} />
-      </a>
-      <a href="mailto:your-email@example.com" className="text-white">
+      </Link>
+      <Link href="mailto:borisnikolic2302@gmail.com" className="text-white">
         <FaEnvelope size={24} />
-      </a>
+      </Link>
     </div>
   );
 }
